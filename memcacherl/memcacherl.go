7d7c5fb4faf2139bf399921979f111111063a415
Alexandre Fiori <fiorix@gmail.com>
2016-03-14 22:09:07 -0400

    Fix for cache miss on increment

--- memcacherl/memcacherl.go
+++ memcacherl/memcacherl.go
@@ -21,20 +21,16 @@ func New(mc *memcache.Client) *Client {
 func (c *Client) Hit(key string, ttlsec int32) (count uint64, remttl int32, err error) {
 	item, err := c.mc.Get(key)
 	if err != nil {
-		if err != memcache.ErrCacheMiss {
-			return 0, 0, err
+		if err == memcache.ErrCacheMiss {
+			return c.create(key, ttlsec)
 		}
-		exp := uint32(time.Now().Unix()) + uint32(ttlsec)
-		item = &memcache.Item{
-			Key:        key,
-			Value:      []byte{'1'},
-			Flags:      exp, // unix time of exp date
-			Expiration: ttlsec,
-		}
-		return 1, ttlsec, c.mc.Set(item)
+		return 0, 0, err
 	}
 	n, err := c.mc.Increment(key, 1)
 	if err != nil {
+		if err == memcache.ErrCacheMiss {
+			return c.create(key, ttlsec)
+		}
 		return 0, 0, err
 	}
 	rem := int32(item.Flags) - int32(time.Now().Unix())
@@ -42,4 +38,19 @@ func (c *Client) Hit(key string, ttlsec int32) (count uint64, remttl int32, err 
 		rem = 0
 	}
 	return n, rem, nil
+}
+
+func (c *Client) create(key string, ttlsec int32) (uint64, int32, error) {
+	exp := uint32(time.Now().Unix()) + uint32(ttlsec)
+	item := &memcache.Item{
+		Key:        key,
+		Value:      []byte{'1'},
+		Flags:      exp, // unix time of exp date
+		Expiration: ttlsec,
+	}
+	err := c.mc.Set(item)
+	if err != nil {
+		return 0, 0, err
+	}
+	return 1, ttlsec, nil
 }
